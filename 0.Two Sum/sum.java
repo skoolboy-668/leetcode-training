a7a56fb62d558f03bcba83dcc21ecb1c4565902a
skoolboy-668 <82486774+skoolboy-668@users.noreply.github.com>
2021-05-27 12:29:58 +0800

    Update sum.java

--- 0.Two Sum/sum.java
+++ 0.Two Sum/sum.java
@@ -1,1 +1,13 @@
 
+class sum {
+    public int[] twoSum(int[] nums, int target) {
+      Map<Integer, Integer> map = new HashMap<>();
+      for(int i = 0; i < nums.length; i++) {
+        if(map.containsKey(target - nums[i])) {
+          return new int[]{map.get(target - nums[i]), i};
+        }
+        map.put(nums[i], i);
+      }
+      return null;
+    }
+}
